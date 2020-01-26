70122ba7ec0514fcb735737cd48fa2b6e0c8d29d
Matt Whitaker <matt@acmwhitaker.co.uk>
2020-01-26 13:45:12 +0000

    Initial Commit

---
+++ Travis-CI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travis-CI
+//
+//  Created by Adrian Whitaker on 26/01/2020.
+//  Copyright © 2020 Matt Whitaker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Travis-CITests/Travis_CITests.swift
@@ -0,0 +1,34 @@
+//
+//  Travis_CITests.swift
+//  Travis-CITests
+//
+//  Created by Adrian Whitaker on 26/01/2020.
+//  Copyright © 2020 Matt Whitaker. All rights reserved.
+//
+
+import XCTest
+@testable import Travis_CI
+
+class Travis_CITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

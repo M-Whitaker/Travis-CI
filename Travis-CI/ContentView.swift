340e4019ad7aebcbcdf3403d6084f5d99cefe92d
Matt Whitaker <matt@acmwhitaker.co.uk>
2020-02-01 19:08:44 +0000

    Refactored code for api calls

--- Travis-CI/ContentView.swift
+++ Travis-CI/ContentView.swift
@@ -7,6 +7,63 @@
 //
 
 import SwiftUI
+import Combine
+
+struct Build {
+    var id: Int
+    var number: String
+    var state: String
+}
+
+struct Branch: Identifiable {
+    var id = UUID()
+    var name: String
+//    var last_build: Build
+}
+
+struct Repository: Identifiable {
+    var id = UUID()
+    var name: String
+    var slug: String
+    var url: String
+    var favourite: Bool
+    var default_branch: Branch
+    var passing: Bool
+    var buildNo: Int
+    var duration: Int
+    var Finished: Int
+}
+
+class NetworkManager: ObservableObject {
+    var didChange = PassthroughSubject<NetworkManager, Never>()
+    
+    var repos = [Repository]() {
+        didSet {
+            didChange.send(self)
+        }
+
+    }
+    
+    init() {
+//        guard let url = URL(string: "api.travis.com") else {return}
+//        URLSession.shared.dataTask(with: url) { (data, _, _) in
+//
+//            guard let data = data else {return}
+//
+//            let repos = try! JSONDecoder().decode([Repository], from: data)
+//            DispatchQueue.main.async {
+//                self.repos = repos
+//            }
+//        }
+        
+        
+        self.repos = [
+                Repository(name: "repo1", slug: "matt43121/repo1", url: "https://example.com", favourite: true, default_branch: Branch(name: "Master"), passing: true, buildNo: 1234, duration: 500, Finished: 1920),
+                Repository(name: "repo2", slug: "matt43121/repo2", url: "https://example2.com", favourite: true, default_branch: Branch(name: "Master"), passing: false, buildNo: 5678, duration: 600, Finished: 1920),
+                Repository(name: "repo3", slug: "matt43121/repo3", url: "https://example3.com", favourite: false, default_branch: Branch(name: "Master"), passing: true, buildNo: 9101112, duration: 1200, Finished: 1920),
+            ]
+    }
+}
 
 struct ContentView: View {
     var body: some View {
@@ -30,42 +87,13 @@ struct ContentView: View {
     }
 }
 
-struct Build {
-    var id: Int
-    var number: String
-    var state: String
-}
-
-struct Branch: Identifiable {
-    var id = UUID()
-    var name: String
-    var last_build: Build
-}
-
-struct Repository: Identifiable {
-    var id = UUID()
-    var name: String
-    var slug: String
-    var url: String
-    var favourite: Bool
-    var default_branch: Branch
-    var passing: Bool
-    var buildNo: Int
-    var duration: Int
-    var Finished: Int
-}
 
 struct HomeView: View {
-    
-    let repos = [
-        Repository(name: "repo1", slug: "matt43121/repo1", url: "https://example.com", favourite: true, default_branch: Branch(name: "Master"), passing: true, buildNo: 1234, duration: 500, Finished: 1920),
-        Repository(name: "repo2", slug: "matt43121/repo2", url: "https://example2.com", favourite: true, default_branch: Branch(name: "Master"), passing: false, buildNo: 5678, duration: 600, Finished: 1920),
-        Repository(name: "repo3", slug: "matt43121/repo3", url: "https://example3.com", favourite: false, default_branch: Branch(name: "Master"), passing: true, buildNo: 9101112, duration: 1200, Finished: 1920),
-    ]
+    @State var networkManager = NetworkManager()
     var body: some View {
             NavigationView {
                 List {
-                    ForEach(repos) { repo in
+                    ForEach(networkManager.repos) { repo in
                         NavigationLink(destination: RepoBuildDetailView(repo: repo)
                                 .navigationBarTitle(Text(repo.name))) {
                         RepoBuildView(repo: repo)

1ce55c778958a8fb2f6efa335b301f920fb0615f
Matt Whitaker <matt@acmwhitaker.co.uk>
2020-02-16 23:38:45 +0000

    Added TravisCI Logo to loading and created README.md

--- Travis-CI/LoadingView.swift
+++ Travis-CI/LoadingView.swift
@@ -17,52 +17,38 @@ struct LoadingView: View {
     
     var body: some View {
         VStack {
-            HStack {
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)))
-                    .offset(y: aquaBounce ? 0 : -150)
-                    .animation(Animation.interpolatingSpring(stiffness: 70, damping: 5).repeatForever(autoreverses: false))
-                    .onAppear() {
-                        self.aquaBounce.toggle()
+            VStack {
+                Image("travis-ci-logo")
+                    .resizable()
+                    .frame(width: 400, height: 300)
+                HStack {
+                    Circle()
+                        .frame(width: 15, height: 15)
+                        .foregroundColor(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+                        .offset(y: aquaBounce ? 0 : -75)
+                        .animation(Animation.interpolatingSpring(stiffness: 140, damping: 5).repeatForever(autoreverses: false))
+                        .onAppear() {
+                            self.aquaBounce.toggle()
+                    }
+                    
+                    Circle()
+                        .frame(width: 15, height: 15)
+                        .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                        .offset(y: blueberryBounce ? 0 : -75)
+                        .animation(Animation.interpolatingSpring(stiffness: 140, damping: 5).repeatForever(autoreverses: false).delay(0.03))
+                        .onAppear() {
+                            self.blueberryBounce.toggle()
+                    }
+                    
+                    Circle()
+                        .frame(width: 15, height: 15)
+                        .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
+                        .offset(y: grapeBounce ? 0 : -75)
+                        .animation(Animation.interpolatingSpring(stiffness: 140, damping: 5).repeatForever(autoreverses: false).delay(0.03*2))
+                        .onAppear() {
+                            self.grapeBounce.toggle()
+                    }
                 }
-                
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)))
-                    .offset(y: blueberryBounce ? 0 : -150)
-                    .animation(Animation.interpolatingSpring(stiffness: 70, damping: 5).repeatForever(autoreverses: false).delay(0.03))
-                    .onAppear() {
-                        self.blueberryBounce.toggle()
-                }
-                
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color(#colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 1)))
-                    .offset(y: grapeBounce ? 0 : -150)
-                    .animation(Animation.interpolatingSpring(stiffness: 70, damping: 5).repeatForever(autoreverses: false).delay(0.03*2))
-                    .onAppear() {
-                        self.grapeBounce.toggle()
-                }
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color(#colorLiteral(red: 1, green: 0.2527923882, blue: 1, alpha: 1)))
-                    .offset(y: magentaBounce ? 0 : -150)
-                    .animation(Animation.interpolatingSpring(stiffness: 70, damping: 5).repeatForever(autoreverses: false).delay(0.03*3))
-                    .onAppear() {
-                        self.magentaBounce.toggle()
-                }
-                
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color(#colorLiteral(red: 1, green: 0.1857388616, blue: 0.5733950138, alpha: 1)))
-                    .offset(y: strawberryBounce ? 0 : -150)
-                    .animation(Animation.interpolatingSpring(stiffness: 70, damping: 5).repeatForever(autoreverses: false).delay(0.03*4))
-                    .onAppear() {
-                        self.strawberryBounce.toggle()
-                }
-                
-               
             }
         }.frame(height: UIScreen.main.bounds.height - 200)
     }
